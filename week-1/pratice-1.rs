1b9970e6d979cc90968882b39558bcaefe9fa6b1
c.monwubaCOS101 <mcchukwuezugo@gmail.com>
2023-10-19 02:26:00 +0100

    (My first commit)

---
+++ week-1/pratice-1.rs
@@ -0,0 +1,16 @@
+fn main(){
+   println!("Welcome to COS 101!")
+   
+   println!("The course learning outcomeis as follows:");
+
+   println!("1. Distinguish between the computer and computer programming,/n
+   	         2. Develop some techniques in computer science,/n 
+   	         3. Understand the diffrent areas of study in computer science,/n 
+   	         4. Conversant with applications of computer science,/n 
+   	         5. Navigate the career prospects in computer science./n 
+           	6. Conversant with computer programming concepts." );
+
+}
+   
+
+
